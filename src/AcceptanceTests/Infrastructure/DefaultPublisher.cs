119303a079a15aa64124f87c83bdcd1254a35d0c
TravisNickels <87037242+TravisNickels@users.noreply.github.com>
2022-03-30 23:11:30 -0700

    First attempt at PubSub test

---
+++ src/AcceptanceTests/Infrastructure/DefaultPublisher.cs
@@ -0,0 +1,16 @@
+﻿namespace NServiceBus.AcceptanceTests.EndpointTemplates
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting.Support;
+
+    public class DefaultPublisher : IEndpointSetupTemplate
+    {
+#pragma warning disable PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+        public Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointConfiguration, Action<EndpointConfiguration> configurationBuilderCustomization)
+#pragma warning restore PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+        {
+            return new DefaultServer().GetConfiguration(runDescriptor, endpointConfiguration, configurationBuilderCustomization);
+        }
+    }
+}
---
+++ src/AcceptanceTests/When_subscribing_to_event_msmq.cs
@@ -0,0 +1,110 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NServiceBus.AcceptanceTests.EndpointTemplates;
+using NServiceBus.Features;
+using NUnit.Framework;
+using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
+
+class When_subscribing_to_event_msmq
+{
+    [Test]
+    public async Task Should_get_the_event()
+    {
+        var routerConfiguration = new MessageRouterConfiguration();
+
+        routerConfiguration.AddTransport(new MsmqTransport())
+            .HasEndpoint(Conventions.EndpointNamingConvention(typeof(Subscriber)));
+
+        routerConfiguration.AddTransport(new LearningTransport())
+            .HasEndpoint(Conventions.EndpointNamingConvention(typeof(Publisher)));
+
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<Publisher>(b => b
+                .When(c => c.SubscriberSubscribed, (session, c) =>
+                {
+                    c.AddTrace("Subscriber is subscribed, going to publish MyEvent");
+
+                    var options = new PublishOptions();
+
+                    return session.Publish(new MyEvent(), options);
+                }))
+            .WithEndpoint<Subscriber>(b => b.When(async (session, ctx) =>
+            {
+                await session.Subscribe<MyEvent>().ConfigureAwait(false);
+                if (ctx.HasNativePubSubSupport)
+                {
+                    ctx.SubscriberSubscribed = true;
+                    ctx.AddTrace("Subscriber is now subscribed (at least we have asked the broker to be subscribed");
+                }
+                else
+                {
+                    ctx.AddTrace("Subscriber has now asked to be subscribed to MyEvent");
+                }
+            }))
+            .WithRouter(routerConfiguration)
+            .Done(c => c.SubscriberGotEvent)
+            .Run().ConfigureAwait(false);
+
+        Assert.IsTrue(context.SubscriberGotEvent);
+    }
+
+    public class Context : ScenarioContext
+    {
+        public bool SubscriberSubscribed { get; set; }
+        public bool SubscriberGotEvent { get; set; }
+    }
+
+    class Publisher : EndpointConfigurationBuilder
+    {
+        public Publisher()
+        {
+            EndpointSetup<DefaultPublisher>(c =>
+            {
+                c.UseTransport(new LearningTransport());
+                //c.ConfigureRouting().RouteToEndpoint(typeof(MyEvent), typeof(Subscriber));
+                c.OnEndpointSubscribed<Context>((s, context) =>
+                {
+                    var subscriber = Conventions.EndpointNamingConvention(typeof(Subscriber));
+                    if (s.SubscriberEndpoint.Contains(subscriber))
+                    {
+                        context.SubscriberSubscribed = true;
+                        context.AddTrace($"{subscriber} is now subscribed");
+                    }
+                });
+                c.DisableFeature<AutoSubscribe>();
+            });
+        }
+    }
+
+    class Subscriber : EndpointConfigurationBuilder
+    {
+        public Subscriber()
+        {
+            EndpointSetup<DefaultServer>(c =>
+            {
+                c.DisableFeature<AutoSubscribe>();
+                c.UseTransport(new MsmqTransport());
+                c.UsePersistence<MsmqPersistence, StorageType.Subscriptions>();
+            }, p => p.RegisterPublisherFor<MyEvent>(typeof(Publisher)));
+        }
+
+        public class MessageHandler : IHandleMessages<MyEvent>
+        {
+            Context context;
+
+            public MessageHandler(Context context) => this.context = context;
+
+            public Task Handle(MyEvent message, IMessageHandlerContext handlerContext)
+            {
+                context.SubscriberGotEvent = true;
+                return Task.FromResult(0);
+            }
+        }
+    }
+
+    public class MyEvent : IEvent
+    {
+    }
+}
+
