025db92da8d46149a4865ff3e41f52cec1c5cf9e
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-01 14:00:27 +0200

    Add example non shared test

---
+++ src/AcceptanceTests/Some_non_shared_test.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+public class Some_non_shared_test : RouterAcceptanceTest
+{
+    [Test]
+    public Task Blah()
+    {
+        return Task.CompletedTask;
+    }
+}
