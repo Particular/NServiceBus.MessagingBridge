e192f857d1a48c3e81aa096e9772e5af0c6f23b1
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-01 09:58:13 +0200

    Msmq

---
+++ src/AcceptanceTests.Msmq/ConfigureMsmqTransportTestExecution.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Transport;
+
+class ConfigureMsmqTransportTestExecution : IConfigureTransportTestExecution
+{
+    public TransportDefinition GetTransportDefinition()
+    {
+        //TODO: need to add a way to configure persistence
+        return new MsmqTransport();
+    }
+
+    public Task Cleanup(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+}
