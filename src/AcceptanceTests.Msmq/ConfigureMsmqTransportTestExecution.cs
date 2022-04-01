884d3f6cc58e50414ea8dd802d6429e481081518
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-01 13:07:47 +0200

    Add msmq cleanup

--- src/AcceptanceTests.Msmq/ConfigureMsmqTransportTestExecution.cs
+++ src/AcceptanceTests.Msmq/ConfigureMsmqTransportTestExecution.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Threading;
 using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Transport;
@@ -7,7 +11,9 @@ class ConfigureMsmqTransportTestExecution : IConfigureTransportTestExecution
 {
     public TransportDefinition GetTransportDefinition()
     {
-        return new MsmqTransport();
+        transportDefinition = new TestableMsmqTransport();
+
+        return transportDefinition;
     }
 
     public void ApplyCustomEndpointConfiguration(EndpointConfiguration endpointConfiguration)
@@ -17,6 +23,45 @@ class ConfigureMsmqTransportTestExecution : IConfigureTransportTestExecution
 
     public Task Cleanup(CancellationToken cancellationToken = default)
     {
+        var allQueues = MessageQueue.GetPrivateQueuesByMachine("localhost");
+        var queuesToBeDeleted = new List<string>();
+
+        foreach (var messageQueue in allQueues)
+        {
+            using (messageQueue)
+            {
+                if (transportDefinition.ReceiveQueues.Any(ra =>
+                {
+                    var indexOfAt = ra.IndexOf("@", StringComparison.Ordinal);
+                    if (indexOfAt >= 0)
+                    {
+                        ra = ra.Substring(0, indexOfAt);
+                    }
+                    return messageQueue.QueueName.StartsWith(@"private$\" + ra, StringComparison.OrdinalIgnoreCase);
+                }))
+                {
+                    queuesToBeDeleted.Add(messageQueue.Path);
+                }
+            }
+        }
+
+        foreach (var queuePath in queuesToBeDeleted)
+        {
+            try
+            {
+                MessageQueue.Delete(queuePath);
+                Console.WriteLine("Deleted '{0}' queue", queuePath);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not delete queue '{0}'", queuePath);
+            }
+        }
+
+        MessageQueue.ClearConnectionCache();
+
         return Task.CompletedTask;
     }
+
+    TestableMsmqTransport transportDefinition;
 }
