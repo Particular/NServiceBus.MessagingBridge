98397ff7298ff4555cdadd4313998fb61411fc2f
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-01 12:33:53 +0200

    Make test names unique

--- src/AcceptanceTests/Shared/Support/RouterAcceptanceTest.cs
+++ src/AcceptanceTests/Shared/Support/RouterAcceptanceTest.cs
@@ -1,4 +1,7 @@
-﻿using NServiceBus.Transport;
+﻿using System.Linq;
+using System.Threading;
+using NServiceBus.AcceptanceTesting.Customization;
+using NServiceBus.Transport;
 using NUnit.Framework;
 
 public class RouterAcceptanceTest
@@ -6,6 +9,21 @@ public class RouterAcceptanceTest
     [SetUp]
     public void SetUp()
     {
+        Conventions.EndpointNamingConvention = t =>
+        {
+            var classAndEndpoint = t.FullName.Split('.').Last();
+
+            var testName = classAndEndpoint.Split('+').First();
+
+            var endpointBuilder = classAndEndpoint.Split('+').Last();
+
+            testName = Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(testName);
+
+            testName = testName.Replace("_", "");
+
+            return testName + "." + endpointBuilder;
+        };
+
         var transportConfig = TestSuiteConfiguration.Current.CreateTransportConfiguration();
 
         TransportBeingTested = transportConfig.GetTransportDefinition();
