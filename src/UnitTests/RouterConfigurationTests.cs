99dcf78a9c0601d82e448b9712ab1121c93ed0e6
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-06 10:06:47 +0200

    Imorove test

--- src/UnitTests/RouterConfigurationTests.cs
+++ src/UnitTests/RouterConfigurationTests.cs
@@ -4,12 +4,19 @@ using NUnit.Framework;
 partial class RouterConfigurationTests
 {
     [Test]
-    public void Subscriber_should_get_the_event()
+    public void Should_require_transports_of_the_same_type_to_be_uniquely_identifiable_by_name()
     {
         var configuration = new RouterConfiguration();
 
-        configuration.AddTransport(new FakeTransport());
 
-        Assert.Throws<InvalidOperationException>(() => configuration.AddTransport(new FakeTransport()));
+        configuration.AddTransport(new SomeTransport(), "some1");
+        configuration.AddTransport(new SomeTransport(), "some2");
+
+        configuration.AddTransport(new SomeOtherTransport());
+
+        Assert.Throws<InvalidOperationException>(() => configuration.AddTransport(new SomeOtherTransport()));
     }
+
+    class SomeTransport : FakeTransport { }
+    class SomeOtherTransport : FakeTransport { }
 }
