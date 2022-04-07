a930d0c65774397cee9626f0e6fb37253cc17d17
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-07 08:41:19 +0200

    Add api approvals

---
+++ src/UnitTests/APIApprovals.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Particular.Approvals;
+using PublicApiGenerator;
+
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    public void PublicApi()
+    {
+        var publicApi = typeof(RouterConfiguration).Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+        });
+
+        Approver.Verify(publicApi);
+    }
+}
