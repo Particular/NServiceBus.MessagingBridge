b44c9a53170d516eae0ac01829c7075b86c02cff
Dennis <dvdstelt@gmail.com>
2022-04-09 00:54:34 +0200

    Added FailedQ header and fixed that headers can be empty.

--- src/NServiceBus.Transport.Bridge/MessageShovel.cs
+++ src/NServiceBus.Transport.Bridge/MessageShovel.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
+using NServiceBus.Raw;
 using NServiceBus.Routing;
 using NServiceBus.Transport;
 
@@ -13,6 +14,7 @@ class MessageShovel
         this.logger = logger;
         this.endpointProxyRegistry = endpointProxyRegistry;
     }
+
     public async Task TransferMessage(
         string localEndpointName,
         QueueAddress queueAddress,
@@ -23,22 +25,36 @@ class MessageShovel
 
         foreach (var targetEndpointProxy in targetEndpointProxies)
         {
-            var messageToSend = new OutgoingMessage(messageContext.NativeMessageId, messageContext.Headers, messageContext.Body);
+            var messageToSend = new OutgoingMessage(messageContext.NativeMessageId, messageContext.Headers,
+                messageContext.Body);
 
             var address = targetEndpointProxy.ToTransportAddress(queueAddress);
 
-            var replyToAddress = messageToSend.Headers[Headers.ReplyToAddress];
-            var replyToLogicalEndpointName = ParseEndpointAddress(replyToAddress);
-            var targetSpecificReplyToAddress = targetEndpointProxy.ToTransportAddress(new QueueAddress(replyToLogicalEndpointName));
-            messageToSend.Headers[Headers.ReplyToAddress] = targetSpecificReplyToAddress;
+            TransformHeader(messageToSend, targetEndpointProxy, Headers.ReplyToAddress);
+            TransformHeader(messageToSend, targetEndpointProxy, "NServiceBus.FailedQ");
 
             var transportOperation = new TransportOperation(messageToSend, new UnicastAddressTag(address));
-            await targetEndpointProxy.Dispatch(new TransportOperations(transportOperation), messageContext.TransportTransaction,
+            await targetEndpointProxy.Dispatch(new TransportOperations(transportOperation),
+                    messageContext.TransportTransaction,
                     cancellationToken)
                 .ConfigureAwait(false);
 
-            logger.LogInformation("Moving the message over to: {0} with a reply to {1}", address, targetSpecificReplyToAddress);
+            messageToSend.Headers.TryGetValue(Headers.ReplyToAddress, out var replyToAddress);
+            logger.LogInformation("Moving the message over to: [{0}] with a reply address of [{1}]", address, replyToAddress);
         }
+    }
+
+    void TransformHeader(OutgoingMessage messageToSend, IRawEndpoint targetEndpointProxy, string header)
+    {
+        if (!messageToSend.Headers.TryGetValue(header, out var replyToAddress))
+        {
+            return;
+        }
+
+        var replyToLogicalEndpointName = ParseEndpointAddress(replyToAddress);
+        var targetSpecificReplyToAddress =
+            targetEndpointProxy.ToTransportAddress(new QueueAddress(replyToLogicalEndpointName));
+        messageToSend.Headers[header] = targetSpecificReplyToAddress;
     }
 
     string ParseEndpointAddress(string replyToAddress)
