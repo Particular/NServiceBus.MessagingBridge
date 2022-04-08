7d14c1e58b4f56eede97dfe381405d24c8cab97c
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-08 16:01:53 +0200

    Fix msmq tests

--- src/AcceptanceTests.Msmq/TestableMsmqTransport.cs
+++ src/AcceptanceTests.Msmq/TestableMsmqTransport.cs
@@ -9,6 +9,11 @@ using NServiceBus.Transport;
 /// </summary>
 class TestableMsmqTransport : MsmqTransport
 {
+    public TestableMsmqTransport()
+    {
+        TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
+    }
+
     public string[] ReceiveQueues = new string[0];
 
     public override async Task<TransportInfrastructure> Initialize(HostSettings hostSettings, ReceiveSettings[] receivers, string[] sendingAddresses, CancellationToken cancellationToken = default)
