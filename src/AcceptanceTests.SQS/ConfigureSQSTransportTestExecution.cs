ca73b92675185d6f368303a31d02ffbebd52c800
Kyle Baley <kyle@baley.org>
2022-04-11 17:45:20 -0500

    WIP

--- src/AcceptanceTests.SQS/ConfigureSQSTransportTestExecution.cs
+++ src/AcceptanceTests.SQS/ConfigureSQSTransportTestExecution.cs
@@ -2,21 +2,20 @@
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.SimpleNotificationService;
 using Amazon.SQS;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
-using NServiceBus.AcceptanceTests;
+//using NServiceBus.AcceptanceTests;
 
 public class ConfigureSQSTransportTestExecution : IConfigureTransportTestExecution
 {
-    string NamePrefix = $"AT{Regex.Replace(Convert.ToBase64String(Guid.NewGuid().ToByteArray()), "[/+=]", "").ToUpperInvariant()}";
+    //string NamePrefix = $"AT{Regex.Replace(Convert.ToBase64String(Guid.NewGuid().ToByteArray()), "[/+=]", "").ToUpperInvariant()}";
 
     public BridgeTransportDefinition GetBridgeTransport()
     {
-        var transportDefinition = new TestableSQSTransport(NamePrefix);
+        var transportDefinition = new TestableSQSTransport(NamePrefixGenerator.GetNamePrefix());
 
         return new BridgeTransportDefinition()
         {
@@ -27,7 +26,7 @@ public class ConfigureSQSTransportTestExecution : IConfigureTransportTestExecuti
 
     public Func<CancellationToken, Task> ConfigureTransportForEndpoint(EndpointConfiguration endpointConfiguration, PublisherMetadata publisherMetadata)
     {
-        var transportDefinition = new TestableSQSTransport(NamePrefix);
+        var transportDefinition = new TestableSQSTransport(NamePrefixGenerator.GetNamePrefix());
         endpointConfiguration.UseTransport(transportDefinition);
 
         return ct => Cleanup(ct);
@@ -42,30 +41,8 @@ public class ConfigureSQSTransportTestExecution : IConfigureTransportTestExecuti
         using (var snsClient = new AmazonSimpleNotificationServiceClient(accessKeyId, secretAccessKey))
         using (var s3Client = new AmazonS3Client(accessKeyId, secretAccessKey))
         {
-            await SQSCleanup.DeleteAllResourcesWithPrefix(sqsClient, snsClient, s3Client, NamePrefix).ConfigureAwait(false);
+            await SQSCleanup.DeleteAllResourcesWithPrefix(sqsClient, snsClient, s3Client, NamePrefixGenerator.GetNamePrefix()).ConfigureAwait(false);
         }
-        //if (cancellationToken.IsCancellationRequested)
-        //{
-        //    transport.EnablePartitioning = true;
-        //}
 
     }
-    public static IAmazonSQS CreateSqsClient()
-    {
-        var credentials = new EnvironmentVariablesAWSCredentials();
-        return new AmazonSQSClient(credentials);
-    }
-
-    public static IAmazonSimpleNotificationService CreateSnsClient()
-    {
-        var credentials = new EnvironmentVariablesAWSCredentials();
-        return new AmazonSimpleNotificationServiceClient(credentials);
-    }
-
-    public static IAmazonS3 CreateS3Client()
-    {
-        var credentials = new EnvironmentVariablesAWSCredentials();
-        return new AmazonS3Client(credentials);
-    }
-
 }
---
+++ src/AcceptanceTests.SQS/NamePrefixGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class NamePrefixGenerator
+{
+
+    static readonly string NamePrefix = $"AT{Regex.Replace(Convert.ToBase64String(Guid.NewGuid().ToByteArray()), "[/+=]", "").ToUpperInvariant()}";
+
+    public static string GetNamePrefix()
+    {
+        return NamePrefix;
+    }
+}
