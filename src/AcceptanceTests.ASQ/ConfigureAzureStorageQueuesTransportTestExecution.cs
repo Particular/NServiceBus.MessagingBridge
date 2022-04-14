c830bb25e7aa08d0f7e824b8282c8341f55c9b11
TravisNickels <87037242+TravisNickels@users.noreply.github.com>
2022-04-13 18:31:41 -0700

    Added ASQ tests

---
+++ src/AcceptanceTests.ASQ/ConfigureAzureStorageQueuesTransportTestExecution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Configuration.AdvancedExtensibility;
+using Testing;
+
+public class ConfigureAzureStorageQueuesTransportTestExecution : IConfigureTransportTestExecution
+{
+    readonly string connectionString = Environment.GetEnvironmentVariable("AzureStorageQueueTransport_ConnectionString");
+    public BridgeTransportDefinition GetBridgeTransport()
+    {
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("No connectionstring for found in environment variable 'AzureStorageQueueTransport_ConnectionString'");
+        }
+
+        var transportDefinition = new TestableAzureStorageQueuesTransport(connectionString)
+        {
+            MessageWrapperSerializationDefinition = new XmlSerializer(),
+            QueueNameSanitizer = BackwardsCompatibleQueueNameSanitizerForTests.Sanitize,
+            Subscriptions = { DisableCaching = true }
+        };
+        transportDefinition.DelayedDelivery.DelayedDeliveryPoisonQueue = "error";
+
+        return new BridgeTransportDefinition()
+        {
+            TransportDefinition = transportDefinition,
+            // new BridgeTransportConfiguration(transportDefinition),
+            Cleanup = (ct) => Cleanup(ct)
+        };
+    }
+
+    public Func<CancellationToken, Task> ConfigureTransportForEndpoint(EndpointConfiguration endpointConfiguration, PublisherMetadata publisherMetadata)
+    {
+        var transportDefinition = new TestableAzureStorageQueuesTransport(connectionString);
+        var errorQueue = endpointConfiguration.GetSettings().GetOrDefault<string>(ErrorQueueSettings.SettingsKey);
+
+        transportDefinition = Utilities.CreateTransportWithDefaultTestsConfiguration(connectionString, delayedDeliveryPoisonQueue: errorQueue);
+        transportDefinition.Subscriptions.DisableCaching = true;
+
+        var routingConfig = endpointConfiguration.UseTransport(transportDefinition);
+
+        foreach (var publisher in publisherMetadata.Publishers)
+        {
+            foreach (var eventType in publisher.Events)
+            {
+                routingConfig.RegisterPublisher(eventType, publisher.PublisherName);
+            }
+        }
+
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+
+        return ct => Cleanup(ct);
+    }
+
+    Task Cleanup(CancellationToken cancellationToken) => Task.CompletedTask;
+}
---
+++ src/AcceptanceTests.ASQ/TestableAzureStorageQueuesTransport.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus;
+
+public class TestableAzureStorageQueuesTransport : AzureStorageQueueTransport
+{
+    public TestableAzureStorageQueuesTransport(string connectionString) : base(connectionString)
+    {
+    }
+
+    //public override async Task<TransportInfrastructure> Initialize(HostSettings hostSettings,
+    //    ReceiveSettings[] receivers,
+    //    string[] sendingAddresses,
+    //    CancellationToken cancellationToken = new CancellationToken())
+    //{
+    //    var infrastructure = await base.Initialize(hostSettings, receivers, sendingAddresses, cancellationToken)
+    //        .ConfigureAwait(false);
+    //    QueuesToCleanup.AddRange(infrastructure.Receivers.Select(x => x.Value.ReceiveAddress).Concat(sendingAddresses)
+    //        .Distinct());
+    //    return infrastructure;
+    //}
+
+    //public List<string> QueuesToCleanup { get; } = new List<string>();
+}
