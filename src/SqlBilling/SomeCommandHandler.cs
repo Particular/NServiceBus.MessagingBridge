eb9f639bf6338d94e05f230de22339b8c3e931fc
Andreas Öhlund <andreas.ohlund@particular.net>
2022-04-18 14:50:48 +0200

    Remove demo projects (#27)

--- src/SqlBilling/SomeCommandHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿class SomeCommandHandler : IHandleMessages<SomeCommand>
-{
-    public Task Handle(SomeCommand message, IMessageHandlerContext context)
-    {
-        Console.WriteLine("Got the message");
-        return Task.CompletedTask;
-    }
-}
