f630ed6791ed2bbd8245bb53b01360fb2868b8bc
Kyle Baley <kyle@baley.org>
2022-04-18 15:14:35 -0500

    Add filters for CI for learning transport and MSMQ transport (#28)

---
+++ src/AcceptanceTesting/TestAttributes/MsmqTestAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
+public class MsmqTestAttribute : Attribute, IApplyToContext
+{
+    public void ApplyToContext(TestExecutionContext context)
+    {
+        var isGithubCI = Environment.GetEnvironmentVariable("CI") == "true";
+        var connectionString = Environment.GetEnvironmentVariable("MSMQTransport");
+        if (isGithubCI && string.IsNullOrWhiteSpace(connectionString))
+        {
+            Assert.Ignore("Ignoring learning transport tests due to unset MSMQTransport environment variable");
+        }
+    }
+}
