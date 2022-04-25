6bccd4a77b60848a61e080595843707a8dc5a1e4
TravisNickels <87037242+TravisNickels@users.noreply.github.com>
2022-04-25 10:42:25 -0700

    Updated sql test name

--- src/AcceptanceTests.SqlServer/MultiSchema/When_custom_schema_configured_for_both_endpoints.cs
+++ src/AcceptanceTests.SqlServer/MultiSchema/When_custom_schema_configured_for_both_endpoints.cs
@@ -7,12 +7,11 @@
     using NUnit.Framework;
     using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
 
-    public class When_custom_schema_configured_for_one_endpoint : BridgeAcceptanceTest
+    public class When_custom_schema_configured_for_both_endpoints : BridgeAcceptanceTest
     {
         const string PublisherSchema = "publisher";
         const string SubscriberSchema = "subscriber";
 
-        //public const string SubscriberSchema = "dbo";
         readonly string connectionString = Environment.GetEnvironmentVariable("SqlServerTransportConnectionString");
 
         [Test]
@@ -79,7 +78,6 @@
                 {
                     var transport = c.ConfigureSqlServerTransport();
                     transport.DefaultSchema = PublisherSchema;
-                    // transport.Subscriptions.DisableCaching = true;
 
                     c.OnEndpointSubscribed<Context>((_, ctx) =>
                     {
@@ -97,11 +95,6 @@
                 {
                     var transport = c.ConfigureSqlServerTransport();
                     transport.DefaultSchema = SubscriberSchema;
-                    // transport.Subscriptions.SubscriptionTableName =
-                    //     new SubscriptionTableName("SubscriptionRouting", "dbo");
-                    // transport.Subscriptions.DisableCaching = true;
-
-                    // c.DisableFeature<AutoSubscribe>();
                 });
             }
 
