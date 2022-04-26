32c8ea193deada60ce6bcb8a366fffb679502f4d
Travis Nickels <87037242+TravisNickels@users.noreply.github.com>
2022-04-26 06:26:49 -0700

    Adding SQL multi-schema tests (#34)
    * Added SQL multi-schema test for publishing
    
    * Updating SQL schema test and fixing some queue cleanup
    
    * cleanup
    
    * Add schemas to the CI for SQL
    
    * Fix schema CI?
    
    * another try at fixing CI
    
    * Some work on Sql tests, but Rider fails on me.
    
    * Added test to add two instances of SQL database
    
    * Added additional nserverbus2 database to CI
    
    * Updated SQL queuesToCleanup
    
    * Ignore case for SQL connection string
    
    * Updated sql test name
    
    * Update connectionstring
    
    * Revert "Updated SQL queuesToCleanup"
    
    This reverts commit f805580d7b394a56ab7e4c4b021b95b3f73136a1.
    
    * Revert "Updated sql test name"
    
    This reverts commit d9c9c7d6a4c9a0c997ada585611615f16d8d77ff.
    
    * checking if CI is causing error
    
    * Added additional nserverbus2 database back into the CI
    
    * Removing test to see if other tests still pass
    
    * Ignore case for SQL connection string
    
    * Added sql schema test back
    
    * Test removing queuesToCleanup
    
    * Added a test for only one custom schema and trying to fix multi-schema test
    
    * Removing schema test for both endpoint to see if single schema test works
    
    * Did some cleanup and added multi custom schema test back
    
    * Simplify endpointsetup for multi schema
    
    * Add default schema for endpoints
    
    * Deleted redundant tests & fixed multi instance test, needs cleanup
    
    * Rebased on master and using TestableBridgeTransport for queue creation
    
    * Removed additional schema creation, we deleted the tests
    
    Co-authored-by: Dennis <dvdstelt@gmail.com>

---
+++ src/AcceptanceTests.SqlServer/SqlTests/Separate_instances.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NUnit.Framework;
+using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
+
+public class Separate_instances : BridgeAcceptanceTest
+{
+    static readonly string connectionString = Environment.GetEnvironmentVariable("SqlServerTransportConnectionString");
+    static string connectionString2 = string.Empty;
+
+    [Test]
+    public async Task Subscriber_should_get_the_event()
+    {
+        if (connectionString.IndexOf("nservicebus", StringComparison.OrdinalIgnoreCase) == -1)
+        {
+            throw new InvalidOperationException(
+                "Can't find database 'nservicebus' in connectionstring. Can't run the test this way.");
+        }
+
+        connectionString2 = connectionString.Replace("nservicebus", "nservicebus2");
+
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<Publisher>(b => b
+                .When(cc => cc.EndpointsStarted, (b, _) =>
+                {
+                    return b.Publish(new MyEvent());
+                }))
+            .WithEndpoint<Subscriber>()
+            .WithBridge(bridgeConfiguration =>
+            {
+                // Publisher SQL Transport
+                var publisherSqlTransport = new TestableSqlServerTransport(connectionString);
+                var publisherBridgeTransport = new TestableBridgeTransport(publisherSqlTransport)
+                {
+                    Name = "publisherBridgeTransport"
+                };
+                // Add endpoints
+                publisherBridgeTransport.AddTestEndpoint<Publisher>();
+
+                // Subscriber Sql Transport
+                var subscriberSqlTransport = new TestableSqlServerTransport(connectionString2);
+                var subscriberBridgeTransport = new TestableBridgeTransport(subscriberSqlTransport)
+                {
+                    Name = "subscriberBridgeTransport"
+                };
+                // Add endpoints
+                var subscriberEndpoint = new BridgeEndpoint(Conventions.EndpointNamingConvention(typeof(Subscriber)));
+                subscriberEndpoint.RegisterPublisher<MyEvent>(Conventions.EndpointNamingConvention(typeof(Publisher)));
+                subscriberBridgeTransport.HasEndpoint(subscriberEndpoint);
+
+                bridgeConfiguration.AddTransport(publisherBridgeTransport);
+                bridgeConfiguration.AddTransport(subscriberBridgeTransport);
+
+                // DTC won't work
+                bridgeConfiguration.RunInReceiveOnlyTransactionMode();
+            })
+            .Done(c => c.SubscriberGotEvent)
+            .Run();
+
+        Assert.IsTrue(context.SubscriberGotEvent);
+    }
+
+    public class Context : ScenarioContext
+    {
+        public bool SubscriberGotEvent { get; set; }
+    }
+
+    class Publisher : EndpointConfigurationBuilder
+    {
+        public Publisher()
+        {
+            EndpointSetup<DefaultPublisher>(c =>
+            {
+                var transport = new SqlServerTransport(connectionString);
+                c.UseTransport(transport);
+            });
+        }
+    }
+
+    class Subscriber : EndpointConfigurationBuilder
+    {
+        public Subscriber()
+        {
+            EndpointSetup<DefaultServer>(c =>
+            {
+                var transport = new SqlServerTransport(connectionString2);
+                c.UseTransport(transport);
+            });
+        }
+
+        public class MessageHandler : IHandleMessages<MyEvent>
+        {
+            Context context;
+
+            public MessageHandler(Context context) => this.context = context;
+
+            public Task Handle(MyEvent message, IMessageHandlerContext handlerContext)
+            {
+                context.SubscriberGotEvent = true;
+                return Task.FromResult(0);
+            }
+        }
+    }
+
+    public class MyEvent : IEvent
+    {
+    }
+}
