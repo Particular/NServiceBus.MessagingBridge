3ee9f109a0fb93b8006b78f22470d1a0a54408d3
Kyle Baley <kyle@baley.org>
2022-05-11 14:21:04 -0500

    Add test for publishing from a handler

---
+++ src/AcceptanceTests.Msmq/Publish_from_handler.cs
@@ -0,0 +1,95 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NUnit.Framework;
+using Conventions = NServiceBus.AcceptanceTesting.Customization.Conventions;
+
+class Publish_from_handler : BridgeAcceptanceTest
+{
+    [Test]
+    public async Task Subscriber_should_get_the_event()
+    {
+        var context = await Scenario.Define<Context>()
+            .WithBridge(bridgeConfiguration =>
+            {
+                var bridgeTransport = new TestableBridgeTransport(TransportBeingTested);
+
+                bridgeTransport.AddTestEndpoint<Publisher>();
+                bridgeConfiguration.AddTransport(bridgeTransport);
+
+                var subscriberEndpoint = new BridgeEndpoint(Conventions.EndpointNamingConvention(typeof(Subscriber)));
+
+                subscriberEndpoint.RegisterPublisher<MyEvent>(Conventions.EndpointNamingConvention(typeof(Publisher)));
+                bridgeConfiguration.AddTestTransportEndpoint(subscriberEndpoint);
+            })
+            .WithEndpoint<Subscriber>(b => b
+                .When(c => TransportBeingTested.SupportsPublishSubscribe || c.SubscriberSubscribed, (session, c) =>
+                {
+                    var sendOptions = new SendOptions();
+                    sendOptions.SetDestination(Conventions.EndpointNamingConvention(typeof(Publisher)));
+                    return session.Send(new PublishEvent(), sendOptions);
+                }))
+            .WithEndpoint<Publisher>()
+            .Done(c => c.SubscriberGotEvent)
+            .Run();
+
+        Assert.IsTrue(context.SubscriberGotEvent);
+    }
+
+    public class Context : ScenarioContext
+    {
+        public bool SubscriberSubscribed { get; set; }
+        public bool SubscriberGotEvent { get; set; }
+    }
+
+    class Publisher : EndpointConfigurationBuilder
+    {
+        public Publisher()
+        {
+            EndpointSetup<DefaultPublisher>(c =>
+            {
+                c.OnEndpointSubscribed<Context>((_, ctx) =>
+                {
+                    ctx.SubscriberSubscribed = true;
+                });
+            });
+        }
+
+        public class MessageHandler : IHandleMessages<PublishEvent>
+        {
+            public Task Handle(PublishEvent message, IMessageHandlerContext handlerContext)
+            {
+                return handlerContext.Publish(new MyEvent());
+            }
+        }
+    }
+
+    class Subscriber : EndpointConfigurationBuilder
+    {
+        public Subscriber()
+        {
+            EndpointSetup<DefaultTestServer>();
+        }
+
+        public class MessageHandler : IHandleMessages<MyEvent>
+        {
+            Context context;
+
+            public MessageHandler(Context context) => this.context = context;
+
+            public Task Handle(MyEvent message, IMessageHandlerContext handlerContext)
+            {
+                context.SubscriberGotEvent = true;
+                return Task.FromResult(0);
+            }
+        }
+    }
+
+    public class MyEvent : IEvent
+    {
+    }
+
+    public class PublishEvent : ICommand
+    {
+    }
+}
