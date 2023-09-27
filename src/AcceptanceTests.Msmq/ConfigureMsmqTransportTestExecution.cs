ea5a9a371218d116828dc36e94bc2965153c4afc
Brandon Ording <bording@gmail.com>
2023-09-27 13:02:45 -0400

    Re-enable MSMQ tests

--- src/AcceptanceTests.Msmq/ConfigureMsmqTransportTestExecution.cs
+++ src/AcceptanceTests.Msmq/ConfigureMsmqTransportTestExecution.cs
@@ -1,83 +1,81 @@
 ﻿using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using MSMQ.Messaging;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using MSMQ.Messaging;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
-//using NServiceBus.Transport;
 
 class ConfigureMsmqTransportTestExecution : IConfigureTransportTestExecution
 {
     public BridgeTransportDefinition GetBridgeTransport()
     {
-        //var transportDefinition = new TestableMsmqTransport();
+        var transportDefinition = new TestableMsmqTransport();
 
         return new BridgeTransportDefinition
         {
-            TransportDefinition = null,//transportDefinition,
-            //Cleanup = (ct) => Cleanup(transportDefinition, ct)
+            TransportDefinition = transportDefinition,
+            Cleanup = (ct) => Cleanup(transportDefinition, ct)
         };
     }
 
     public Func<CancellationToken, Task> ConfigureTransportForEndpoint(EndpointConfiguration endpointConfiguration, PublisherMetadata publisherMetadata)
     {
-        //var transportDefinition = new TestableMsmqTransport();
-        //var routingConfig = endpointConfiguration.UseTransport(transportDefinition);
-        //endpointConfiguration.UsePersistence<MsmqPersistence, StorageType.Subscriptions>();
+        var transportDefinition = new TestableMsmqTransport();
+        var routingConfig = endpointConfiguration.UseTransport(transportDefinition);
+        endpointConfiguration.UsePersistence<MsmqPersistence, StorageType.Subscriptions>();
 
-        //foreach (var publisher in publisherMetadata.Publishers)
-        //{
-        //    foreach (var eventType in publisher.Events)
-        //    {
-        //        routingConfig.RegisterPublisher(eventType, publisher.PublisherName);
-        //    }
-        //}
+        foreach (var publisher in publisherMetadata.Publishers)
+        {
+            foreach (var eventType in publisher.Events)
+            {
+                routingConfig.RegisterPublisher(eventType, publisher.PublisherName);
+            }
+        }
 
-        //return (ct) => Cleanup(transportDefinition, ct);
-        return (_) => Task.CompletedTask;
+        return (ct) => Cleanup(transportDefinition, ct);
     }
 
-    //static Task Cleanup(TestableMsmqTransport msmqTransport, CancellationToken cancellationToken)
-    //{
-    //    var allQueues = MessageQueue.GetPrivateQueuesByMachine("localhost");
-    //    var queuesToBeDeleted = new List<string>();
+    static Task Cleanup(TestableMsmqTransport msmqTransport, CancellationToken cancellationToken)
+    {
+        var allQueues = MessageQueue.GetPrivateQueuesByMachine("localhost");
+        var queuesToBeDeleted = new List<string>();
 
-    //    foreach (var messageQueue in allQueues)
-    //    {
-    //        using (messageQueue)
-    //        {
-    //            if (msmqTransport.ReceiveQueues.Any(ra =>
-    //            {
-    //                var indexOfAt = ra.IndexOf("@", StringComparison.Ordinal);
-    //                if (indexOfAt >= 0)
-    //                {
-    //                    ra = ra.Substring(0, indexOfAt);
-    //                }
-    //                return messageQueue.QueueName.StartsWith(@"private$\" + ra, StringComparison.OrdinalIgnoreCase);
-    //            }))
-    //            {
-    //                queuesToBeDeleted.Add(messageQueue.Path);
-    //            }
-    //        }
-    //    }
+        foreach (var messageQueue in allQueues)
+        {
+            using (messageQueue)
+            {
+                if (msmqTransport.ReceiveQueues.Any(ra =>
+                {
+                    var indexOfAt = ra.IndexOf("@", StringComparison.Ordinal);
+                    if (indexOfAt >= 0)
+                    {
+                        ra = ra.Substring(0, indexOfAt);
+                    }
+                    return messageQueue.QueueName.StartsWith(@"private$\" + ra, StringComparison.OrdinalIgnoreCase);
+                }))
+                {
+                    queuesToBeDeleted.Add(messageQueue.Path);
+                }
+            }
+        }
 
-    //    foreach (var queuePath in queuesToBeDeleted)
-    //    {
-    //        try
-    //        {
-    //            MessageQueue.Delete(queuePath);
-    //            Console.WriteLine("Deleted '{0}' queue", queuePath);
-    //        }
-    //        catch (Exception)
-    //        {
-    //            Console.WriteLine("Could not delete queue '{0}'", queuePath);
-    //        }
-    //    }
+        foreach (var queuePath in queuesToBeDeleted)
+        {
+            try
+            {
+                MessageQueue.Delete(queuePath);
+                Console.WriteLine("Deleted '{0}' queue", queuePath);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not delete queue '{0}'", queuePath);
+            }
+        }
 
-    //    MessageQueue.ClearConnectionCache();
+        MessageQueue.ClearConnectionCache();
 
-    //    return Task.CompletedTask;
-    //}
+        return Task.CompletedTask;
+    }
 }
