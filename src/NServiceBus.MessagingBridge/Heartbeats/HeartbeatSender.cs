b3829549b72e1232aff5a7d201ee8aebf04b7f7d
Adam Wright <awright18@gmail.com>
2024-02-02 13:25:42 -0600

    Add code from NServiceBus.Heartbeats using full heartbeat endpoints

---
+++ src/NServiceBus.MessagingBridge/Heartbeats/HeartbeatSender.cs
@@ -0,0 +1,109 @@
+﻿namespace NServiceBus.MessagingBridge.Heartbeats;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hosting;
+using ServiceControl.Plugin.Heartbeat.Messages;
+using Transport;
+
+/// <summary>
+/// 
+/// </summary>
+public class HeartbeatSender : IDisposable
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="dispatcher"></param>
+    /// <param name="hostInfo"></param>
+    /// <param name="backend"></param>
+    /// <param name="endpointName"></param>
+    /// <param name="interval"></param>
+    /// <param name="timeToLive"></param>
+    public HeartbeatSender(IMessageDispatcher dispatcher, HostInformation hostInfo,
+        ServiceControlBackend backend, string endpointName, TimeSpan interval, TimeSpan timeToLive)
+    {
+        this.dispatcher = dispatcher;
+        this.hostInfo = hostInfo;
+        this.backend = backend;
+        this.endpointName = endpointName;
+        this.interval = interval;
+        this.timeToLive = timeToLive;
+    }
+
+    async Task SendHeartbeatsAndSwallowExceptions(CancellationToken cancellationToken)
+    {
+        Logger.Debug($"Start sending heartbeats every {interval}");
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                await Task.Delay(interval, cancellationToken).ConfigureAwait(false);
+
+                var message = new EndpointHeartbeat
+                {
+                    ExecutedAt = DateTime.UtcNow,
+                    EndpointName = endpointName,
+                    Host = hostInfo.DisplayName,
+                    HostId = hostInfo.HostId
+                };
+
+                await backend.Send(message, timeToLive, dispatcher, cancellationToken).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException ex) when (cancellationToken.IsCancellationRequested)
+            {
+                // private token, sender is being stopped, log the exception in case the stack trace is ever needed for debugging
+                Logger.Debug("Operation canceled while stopping heartbeat sending.", ex);
+                break;
+            }
+            catch (Exception ex)
+            {
+                Logger.Warn("Unable to send heartbeat to ServiceControl.", ex);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task Start(CancellationToken cancellationToken)
+    {
+        stoppingCancelationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+        sendHeartBeatsTask = SendHeartbeatsAndSwallowExceptions(stoppingCancelationTokenSource.Token);
+
+        if (sendHeartBeatsTask.IsCompleted)
+        {
+            return sendHeartBeatsTask;
+        }
+
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    public async Task Stop(CancellationToken cancellationToken)
+    {
+        stoppingCancelationTokenSource!.Cancel();
+    }
+
+    CancellationTokenSource stoppingCancelationTokenSource;
+    Task sendHeartBeatsTask;
+    readonly IMessageDispatcher dispatcher;
+    readonly HostInformation hostInfo;
+    readonly ServiceControlBackend backend;
+    readonly TimeSpan interval;
+    readonly TimeSpan timeToLive;
+    readonly string endpointName;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Dispose() => stoppingCancelationTokenSource.Cancel();
+}
---
+++ src/NServiceBus.MessagingBridge/Heartbeats/ServiceControlBackend.cs
@@ -0,0 +1,61 @@
+﻿namespace NServiceBus.MessagingBridge.Heartbeats;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Performance.TimeToBeReceived;
+using Routing;
+using Transport;
+
+public class ServiceControlBackend
+{
+    public ServiceControlBackend(string destinationQueue, ReceiveAddresses receiveAddresses)
+    {
+        this.destinationQueue = destinationQueue;
+        this.receiveAddresses = receiveAddresses;
+    }
+
+    public Task Send(object messageToSend, TimeSpan timeToBeReceived, IMessageDispatcher dispatcher,
+        CancellationToken cancellationToken = default)
+    {
+        var body = Serialize(messageToSend);
+        return Send(body, messageToSend.GetType().FullName, timeToBeReceived, dispatcher, cancellationToken);
+    }
+
+    internal static byte[] Serialize(object messageToSend)
+    {
+        // return Encoding.UTF8.GetBytes(SimpleJson.SerializeObject(messageToSend, serializerStrategy));
+    }
+
+    Task Send(byte[] body, string messageType, TimeSpan timeToBeReceived, IMessageDispatcher dispatcher,
+        CancellationToken cancellationToken)
+    {
+        var headers = new Dictionary<string, string>
+        {
+            [Headers.EnclosedMessageTypes] = messageType,
+            [Headers.ContentType] = ContentTypes.Json,
+            [Headers.MessageIntent] = sendIntent
+        };
+
+        if (receiveAddresses != null)
+        {
+            headers[Headers.ReplyToAddress] = receiveAddresses.MainReceiveAddress;
+        }
+
+        var outgoingMessage = new OutgoingMessage(Guid.NewGuid().ToString(), headers, body);
+        var properties = new DispatchProperties
+        {
+            DiscardIfNotReceivedBefore = new DiscardIfNotReceivedBefore(timeToBeReceived)
+        };
+        var operation = new TransportOperation(outgoingMessage, new UnicastAddressTag(destinationQueue), properties);
+        return dispatcher.Dispatch(new TransportOperations(operation), new TransportTransaction(), cancellationToken);
+    }
+
+    readonly string sendIntent = MessageIntent.Send.ToString();
+    string destinationQueue;
+    readonly ReceiveAddresses receiveAddresses; // note that ReceiveAddresses will be null on send-only endpoints
+
+    static IJsonSerializerStrategy serializerStrategy = new MessageSerializationStrategy();
+}
