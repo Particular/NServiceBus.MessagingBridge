390a8b0302737e1ee5ebf3a2e8c1a7173e5f188c
Adam Wright <awright18@gmail.com>
2024-02-20 10:20:40 -0600

    add test console

---
+++ src/NServiceBus.MessagingBridge.Console/NServiceBus.MessagingBridge.Console/PassingCustomCheck.cs
@@ -0,0 +1,15 @@
+﻿namespace NServiceBus;
+
+public class PassingCustomCheck : CustomCheck
+{
+    public PassingCustomCheck()
+        : base("PassingCheck", "Test", TimeSpan.FromSeconds(1))
+    {
+    }
+
+    public override Task<CheckResult> PerformCheck(CancellationToken cancellationToken = default) =>
+        Task.FromResult(new CheckResult()
+        {
+            HasFailed = false
+        });
+}
---
+++ src/NServiceBus.MessagingBridge.Console/NServiceBus.MessagingBridge.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+var app = Host.CreateDefaultBuilder()
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddSimpleConsole(options =>
+        {
+            options.IncludeScopes = false;
+            options.SingleLine = true;
+            options.TimestampFormat = "hh:mm:ss ";
+        });
+    })
+    .UseNServiceBusBridge((ctx, bridgeConfiguration) =>
+    {
+        var n1 = new BridgeTransport(
+            new SqlServerTransport(@"Server=.\SqlExpress;Database=N1;Integrated Security=true;")
+            {
+                TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+            })
+        {
+            Name = $"SQL-N1",
+            AutoCreateQueues = true
+        };
+
+        n1.SendHeartbeatTo("error");
+
+        n1.ReportCustomChecksTo("error");
+
+        n1.HasEndpoint("N1");
+
+        var n2 = new BridgeTransport(
+            new SqlServerTransport(@"Server=.\SqlExpress;Database=N2;Integrated Security=true;")
+            {
+                TransportTransactionMode = TransportTransactionMode.SendsAtomicWithReceive
+            })
+        {
+            Name = $"SQL-N2",
+            AutoCreateQueues = true
+        };
+
+        n2.SendHeartbeatTo("error");
+
+        n2.ReportCustomChecksTo("error");
+
+        n2.HasEndpoint("N2");
+
+        bridgeConfiguration.AddTransport(n1);
+        bridgeConfiguration.AddTransport(n2);
+    })
+    .Build();
+
+await app.RunAsync()
+    .ConfigureAwait(false);
