d7d8bb87b3bdda6237c89c223cd8c643949ab295
Adam Wright <awright18@gmail.com>
2024-02-22 19:26:13 -0600

    Add CustomChecks

---
+++ src/NServiceBus.MessagingBridge/Configuration/CustomChecksConfiguration.cs
@@ -0,0 +1,13 @@
+namespace NServiceBus
+{
+    using System;
+
+    class CustomChecksConfiguration
+    {
+        public string ServiceControlQueue { get; set; }
+
+        public TimeSpan? TimeToLive { get; set; }
+
+        public Type[] CustomCheckTypes { get; set; }
+    }
+}
---
+++ src/NServiceBus.MessagingBridge/CustomChecks/CustomCheckRegistrar.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus
+{
+    using System.Linq;
+    using Hosting.Helpers;
+    using Microsoft.Extensions.DependencyInjection;
+
+    static class CustomCheckRegistrar
+    {
+        internal static IServiceCollection AddCustomChecks(this IServiceCollection services)
+        {
+            var customChecks = new AssemblyScanner()
+                .GetScannableAssemblies()
+                .Types
+                .Where(type =>
+                    (type.IsAssignableFrom(typeof(ICustomCheck)) || type.IsSubclassOf(typeof(CustomCheck)))
+                    && !type.Name.Equals("ICustomCheck")
+                    && !type.Name.Equals("CustomCheck"));
+
+            foreach (var customCheck in customChecks)
+            {
+                services.AddTransient(typeof(ICustomCheck), customCheck);
+            }
+
+            return services;
+        }
+    }
+}
