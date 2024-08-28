acae0d1e90b9415e586cc9d2f32061ae7a9e6efc
Andreas Öhlund <andreas.ohlund@particular.net>
2024-08-28 08:51:00 +0200

    Simplify

--- src/AcceptanceTests.Msmq/Remote_addresses.cs
+++ src/AcceptanceTests.Msmq/Remote_addresses.cs
@@ -12,117 +12,71 @@ public class Remote_addresses : BridgeAcceptanceTest
     public async Task Should_get_the_reply()
     {
         var ctx = await Scenario.Define<Context>()
-                    .WithEndpoint<SendingEndpoint>(c => c
-                        .When(cc => cc.EndpointsStarted, (b, _) => b.SendLocal(new StartMessage())))
-                    .WithEndpoint<ReplyingEndpoint>()
-                    .WithEndpoint<ReplyReceivingEndpoint>()
-                    .WithBridge(bridgeConfiguration =>
-                    {
-                        var bridgeTransport = new TestableBridgeTransport(TransportBeingTested);
+            .WithEndpoint<SendingEndpoint>(c => c
+                .When(cc => cc.EndpointsStarted, (b, _) => b.Send(new MyMessage())))
+            .WithEndpoint<RemoteEndpoint>()
+            .WithBridge(bridgeConfiguration =>
+            {
+                var bridgeTransport = new TestableBridgeTransport(TransportBeingTested);
 
-                        bridgeTransport.AddTestEndpoint<SendingEndpoint>();
+                var receivingEndpointName =
+                    Conventions.EndpointNamingConvention(typeof(RemoteEndpoint));
 
-                        var replyReceivingEndpointName =
-                            Conventions.EndpointNamingConvention(typeof(ReplyReceivingEndpoint));
-                        var replyReceivingEndpoint = new BridgeEndpoint(replyReceivingEndpointName, replyReceivingEndpointName + "@someserver");
+                var receivingEndpoint =
+                    new BridgeEndpoint(receivingEndpointName, receivingEndpointName + "@localhost");
 
-                        bridgeTransport.HasEndpoint(replyReceivingEndpoint);
+                bridgeTransport.HasEndpoint(receivingEndpoint);
 
-                        bridgeConfiguration.AddTransport(bridgeTransport);
+                bridgeConfiguration.AddTransport(bridgeTransport);
 
-                        bridgeConfiguration.AddTestTransportEndpoint<ReplyingEndpoint>();
-                    })
-                    .Done(c => c.SendingEndpointGotResponse)
-                    .Run();
+                bridgeConfiguration.AddTestTransportEndpoint<SendingEndpoint>();
+            })
+            .Done(c => c.ReceivingEndpointGotMessage)
+            .Run();
 
-        Assert.That(ctx.SendingEndpointGotResponse, Is.True);
+        Assert.That(ctx.ReceivingEndpointGotMessage, Is.True);
     }
 
     public class Context : ScenarioContext
     {
-        public bool SendingEndpointGotResponse { get; set; }
+        public bool ReceivingEndpointGotMessage { get; set; }
     }
 
     public class SendingEndpoint : EndpointConfigurationBuilder
     {
         public SendingEndpoint()
         {
-            EndpointSetup<DefaultServer>(c =>
+            EndpointSetup<DefaultTestServer>(c =>
             {
-                c.ConfigureRouting().RouteToEndpoint(typeof(MyMessage), typeof(ReplyingEndpoint));
+                c.ConfigureRouting().RouteToEndpoint(typeof(MyMessage), typeof(RemoteEndpoint));
             });
-        }
-
-        public class ResponseHandler : IHandleMessages<StartMessage>
-        {
-            readonly ITransportAddressResolver transportAddressResolver;
-
-            public ResponseHandler(ITransportAddressResolver transportAddressResolver)
-            {
-                this.transportAddressResolver = transportAddressResolver;
-            }
-
-            public Task Handle(StartMessage message, IMessageHandlerContext context)
-            {
-                var sendOptions = new SendOptions();
-                var endpointName = NServiceBus.AcceptanceTesting.Customization.Conventions.EndpointNamingConvention(typeof(ReplyReceivingEndpoint));
-                var endpointAddress = transportAddressResolver.ToTransportAddress(new QueueAddress(endpointName));
-                sendOptions.RouteReplyTo(endpointAddress);
-
-                return context.Send(new MyMessage(), sendOptions);
-            }
         }
     }
 
-    public class ReplyReceivingEndpoint : EndpointConfigurationBuilder
+    public class RemoteEndpoint : EndpointConfigurationBuilder
     {
-        public ReplyReceivingEndpoint()
+        public RemoteEndpoint()
         {
             EndpointSetup<DefaultServer>();
         }
 
-        public class ResponseHandler : IHandleMessages<MyReply>
+        public class MessageHandler : IHandleMessages<MyMessage>
         {
-            public ResponseHandler(Context context)
+            readonly Context testContext;
+
+            public MessageHandler(Context context)
             {
                 testContext = context;
             }
-
-            public Task Handle(MyReply messageThatIsEnlisted, IMessageHandlerContext context)
-            {
-                testContext.SendingEndpointGotResponse = true;
-                return Task.CompletedTask;
-            }
-
-            Context testContext;
-        }
-    }
-
-    public class ReplyingEndpoint : EndpointConfigurationBuilder
-    {
-        public ReplyingEndpoint()
-        {
-            EndpointSetup<DefaultTestServer>();
-        }
-
-        public class MessageHandler : IHandleMessages<MyMessage>
-        {
             public Task Handle(MyMessage message, IMessageHandlerContext context)
             {
-                return context.Reply(new MyReply());
+                testContext.ReceivingEndpointGotMessage = true;
+                return Task.CompletedTask;
             }
         }
-    }
-
-    public class StartMessage : IMessage
-    {
     }
 
     public class MyMessage : IMessage
-    {
-    }
-
-    public class MyReply : IMessage
     {
     }
 }
