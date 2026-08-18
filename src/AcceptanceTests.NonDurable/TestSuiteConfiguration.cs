ef463cfeaba957076afbd520275cde337661666f
David Boike <david.boike@gmail.com>
2026-08-18 00:14:53 -0500

    Add NonDurable transport tests to bridge (#895)
    * Add NonDurable transport tests to bridge
    
    * Make non-durable tests parallelizable by fixture
    
    * Set up WSL and Azure login only where needed

---
+++ src/AcceptanceTests.NonDurable/TestSuiteConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+[assembly: LearningTransportTest]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+public partial class TestSuiteConfiguration
+{
+    public IConfigureTransportTestExecution CreateTransportConfiguration() => new ConfigureNonDurableTransportTestExecution();
+}
